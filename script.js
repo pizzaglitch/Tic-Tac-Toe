124bce26cd6575e1ccd5338b43abce98c15817f1
Garrett Hufstedler <garretthufstedler@gmail.com>
2022-10-15 02:18:08 -0400

    Update displayPlayerSelection to properly add the player's choice to DOM and gameBoard array.

--- script.js
+++ script.js
@@ -1,25 +1,67 @@
 const gameGrid = document.querySelector('#gameGrid');
+const gameBox = document.querySelectorAll('.gameBox');
+
 //Gameboard Object 
 const gameBoardObject = {
-    gameBoard: [topLeft, topMid, topRight, midLeft, midMid, midRight, botLeft, botMid, botRight],
+    gameBoard: ['','','',
+                '','','',
+                '','',''],
+    player: '',
+    opponent: '',
     displayGameBoard: function() {
-        for (let i = 0; i < gameBoard.length; i++) {
-            let newDiv = document.createElement('div');
-            newDiv.id = 'div' + i; 
-            newDiv.className = 'gameBox';
-            gameGrid.appendChild(newDiv); 
+        //Place gameBoard array elements inside divs
+        // for (let i = 0; i < gameBoardObject.gameBoard.length; i++) {
+        //     const newDiv = document.createElement('div');
+        //     newDiv.id = i; 
+        //     newDiv.className = 'gameBox';
+        // //Adds event listener to each div. Checks if id == index, adds player selection if so
+            
+        //     // newDiv.addEventListener('click', (e) => {
+        //     //     const selectDivId = e.target.id; 
+        //     //         for (let i=0; i < gameBoardObject.gameBoard.length; i++) {
+        //     //             if (gameBoardObject.gameBoard[i] == selectDivId) {
+        //     //                 gameBoardObject.gameBoard[i] = gameBoardObject.player
+        //     //             } 
+        //     //         }   
+                   
+        //     // });
+        //     gameGrid.appendChild(newDiv);
+        // }
+    },
+    displayPlayerSelection: function(event) {
+        const selectedDiv = event.currentTarget;
+        const selectedDivId = event.currentTarget.id; 
+        selectedDiv.innerText = gameBoardObject.player;
+        for (let i=0; i < gameBoardObject.gameBoard.length; i++) {
+            if (i == selectedDivId) {
+                gameBoardObject.gameBoard[i] = gameBoardObject.player;
+            } 
         }
     }
 };
-
 gameBoardObject.displayGameBoard();
 
+/* 
+console.log(gameBoardObject.gameBoard)
+console.log(gameBoardObject.displayPlayerSelection.selectDivId) 
+*/
+
+// player creator
+const playerFactory = (choice, score) => {
+    this.choice = choice;
+
+        return {choice, score};
+}
+
+//Display 
 const displayController = {
     score: '',
+    playerSelectX: function() {
+        gameBoardObject.player = 'x';
+        gameBoardObject.opponent = 'o';
+    },
+    playerSelectO: function() {
+        gameBoardObject.player = 'o';
+        gameBoardObject.opponent = 'x';
+    }
 }
-
-const playerFactory = (playerName, score) => {
-    const playerCreator = () => console.log(`Welcome, ${playerName}!`);
-    return { playerName, score, playerCreator };
-}
-
