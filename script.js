712289794810a1e7058fcf728349eb60e7b4181a
Garrett Hufstedler <garretthufstedler@gmail.com>
2022-10-25 16:39:18 -0400

    Add logic to game declaration to declare a tie.

--- script.js
+++ script.js
@@ -82,67 +82,18 @@ const displayController = {
         const winArray = this.winConditions;
         const player = gameBoardObject.player;
         const opponent = gameBoardObject.opponent;
-        let winner = '';
-        // if(winner) {
-        //     alert(winner + 'wins!');
-        // }
-        // for(let [a, b, c,] of this.winConditions) {
-        //     if(board[a] && board[b] === board[b] && board[a] === board[c]) {
-        //         return board[a];
-        //     }
-        // }
-        
-        // return '';
-
-        /// WIP
-        // for (let i=0; i < boardArray.length; i++) {
-            // if (array[0] == array[1] && array[2] === player ||
-            //     array[3] && array[4] && array[5] === player ||
-            //     array[6] && array[7] && array[8] === player) {
-            //         console.log(`${player} wins!`); 
-            // } 
-        // }
-
-        //gameWinning check 
-        // for (let i=0; i < winArray.length; i++) {
-        //     for (let j=0; j < winArray[i].length; j++) {
-        //         if(boardArray[i[0]] === boardArray[i[1]] && boardArray[i[1]] === boardArray[i[2]]) {
-        //             console.log(`${player} wins`)
-        //             break;
-        //         }
-        //     }
-        // }
+        let winner = document.getElementById('header').innerText;
 
         this.winConditions.forEach(i=>{
             if(boardArray[i[0]].length > 0 && boardArray[i[0]] === boardArray[i[1]] && boardArray[i[1]] === boardArray[i[2]]) {
                 console.log(boardArray[i[0]]);
                 document.getElementById('header').innerText = `The winner is ${boardArray[i[0]]}`;
+            } else if (!boardArray.includes('')) {
+                document.getElementById('header').innerText = "It's a tie.";
+            //needs to check for a tie below
+            // else if (boardArray[i] !== '') {
+            //     document.getElementById('header').innerText = "It's a tie.";
             }
         })
-
-
-            // else if 
-            //     (array[0] && array[4] && array[8] === player ||
-            //     array[3] && array[5] && array[7] === player ||
-            //     array[0] && array[3] && array[6] === player ||
-            //     array[1] && array[4] && array[7] === player ||
-            //     array[2] && array[5] && array[8] === player) {
-            //     console.log(`${player} wins!`); 
-            // } 
-            // else if 
-            //     (array[0] && array[1] && array[2] === opponent ||
-            //     array[3] && array[4] && array[5] === opponent ||
-            //     array[6] && array[7] && array[8] === opponent) {
-            //         console.log(`${opponent} wins!`); 
-            // } else if
-            //     (array[0] && array[4] && array[8] === opponent ||
-            //     array[3] && array[5] && array[7] === opponent ||
-            //     array[0] && array[3] && array[6] === opponent ||
-            //     array[1] && array[4] && array[7] === opponent ||
-            //     array[2] && array[5] && array[8] === opponent) {
-            //     console.log(`${opponent} wins!`); 
-            // } 
-        // }
     }
 }
-// == player && array[3] == player && array[6] == player
